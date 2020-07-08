09e1ff13152bc1b8cc2cb9282864bd32e619e61b
ian-nam <ian.nam@navercorp.com>
2020-07-08 16:05:42 +0900

    Add bloomfilter, pagerank

--- build.sbt
+++ build.sbt
@@ -2,12 +2,15 @@ name := "spark-snippet"
 
 version := "0.1"
 
-
-
 val sparkVersion = "3.0.0"
 val scalatestVersion = "3.2.0"
+val scaldingVersion = "0.17.4"
+val algebirdVersion = "0.13.7"
 
 scalaVersion := "2.12.11"
+
+javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
+
 scalacOptions ++= Seq(
     "-target:jvm-1.8",
     "-deprecation",
@@ -15,11 +18,14 @@ scalacOptions ++= Seq(
     "-unchecked",
     "-language:higherKinds"
 )
-javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
+
+resolvers += "Cascading libraries" at "https://conjars.org/repo"
 
 libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-mllib" % sparkVersion,
+    "com.twitter" %% "scalding-core" % scaldingVersion,
+    "com.twitter" %% "algebird-spark" % algebirdVersion,
     "org.scalatest" %% "scalatest" % scalatestVersion % "test",
 )
 
--- src/main/scala/kr/iannam/spark/snippet/Basics.scala
+++ src/main/scala/kr/iannam/spark/snippet/Basics.scala
@@ -1,5 +1,7 @@
 package kr.iannam.spark.snippet
 
+import com.twitter.algebird.{BloomFilter, BloomFilterAggregator}
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.rdd.MLPairRDDFunctions._
 
@@ -7,10 +9,9 @@ import org.apache.spark.mllib.rdd.MLPairRDDFunctions._
  * @author 남상욱 (ian.nam@navercorp.com)
  */
 object Basics {
+    case class Simple(value: String)
     case class Quotes(word: String, count: Double)
     case class User(age: Int, income: Double, score: Double)
-    case class Stats(max: Double, min: Double, mean: Double, stddev: Double)
-    case class UserStats(age: Stats, income: Stats, score: Stats)
 
     def sum(input: RDD[Quotes]): RDD[(String, Double)] = {
         input.map(x => (x.word, x.count)).reduceByKey(_ + _)
@@ -40,9 +41,12 @@ object Basics {
         input.map(x => (x.word, x.count)).top(n)(Ordering.by(_._2)).map(x => Quotes(x._1, x._2))
     }
 
-    def groupedTopN(input: RDD[Quotes], n: Int): RDD[Quotes] = {
-        input.groupBy(_.word).values.flatMap(_.toList.sortBy(-_.count).take(n)) // case 1
-        input.keyBy(_.word).topByKey(n)(Ordering.by(_.count)).flatMap(_._2) // case 2
+    def groupedTopN1(input: RDD[Quotes], n: Int): RDD[Quotes] = {
+        input.groupBy(_.word).values.flatMap(_.toList.sortBy(-_.count).take(n))
+    }
+
+    def groupedTopN2(input: RDD[Quotes], n: Int): RDD[Quotes] = {
+        input.keyBy(_.word).topByKey(n)(Ordering.by(_.count)).flatMap(_._2)
     }
 
     def countDistinct(input: RDD[Quotes]): Long = {
@@ -51,5 +55,25 @@ object Basics {
 
     def countApprox(input: RDD[Quotes]): Long = {
         input.map(_.word).countApproxDistinct()
+    }
+
+    def bloomFilter(lhs: RDD[String], rhs: RDD[String]): RDD[String] = {
+        import com.twitter.algebird.spark._
+
+        val numEntries = lhs.countApprox(timeout = 100, confidence = 0.95F).getFinalValue().mean.toInt
+        val width = BloomFilter.optimalWidth(numEntries = numEntries, fpProb = 0.05D).get
+        val numHashes = BloomFilter.optimalNumHashes(numEntries, width)
+        val bf = rhs.algebird.aggregate(BloomFilterAggregator(numHashes, width))
+        lhs.filter(s => bf.contains(s).isTrue)
+    }
+
+    @transient lazy val sf = new SparkConf().setMaster("local[*]").setAppName("spark-snippet")
+    @transient lazy val sc = new SparkContext(sf)
+    def main(args: Array[String]): Unit = {
+
+        val lhs = (1 to 10000000).toList.map(_.toString)
+        val rhs = (1000 to 1100).toList.map(_.toString)
+
+        bloomFilter(sc.parallelize(lhs), sc.parallelize(rhs)).foreach(println)
     }
 }
